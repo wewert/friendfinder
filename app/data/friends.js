50513909ee9c169eff4cdc6da30fa0e3be1890eb
Ken Lee <wewert@gmail.com>
2018-03-04 23:39:00 -0700

    damn it

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,164 @@
-var requirer = require('requirer');
-var express = require('request');
-var bodyParser = require('body-parser');
+var friendList = [
+  {
+    name: 'The Colossus',
+    image: 'http://l7.alamy.com/zooms/79087bebb44546588230fb965f9bf922/the-colossus-of-new-york-1958-eugene-lourie-dir-clny-002-moviestore-danafj.jpg',
+    scores: [
+      "5",
+      "4",
+      "4",
+      "3",
+      "5",
+      "4",
+      "5",
+      "5",
+      "2",
+      "5"
+    ]
+  },
+  {
+    name: 'Tobor',
+    image: 'https://s-media-cache-ak0.pinimg.com/originals/e0/10/2b/e0102b85cc33824f9240d5d2239914d4.jpg',
+    scores: [
+      "3",
+      "4",
+      "4",
+      "1",
+      "3",
+      "4",
+      "3",
+      "3",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: 'Chani',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPtakrhR1BqD1tpFGo04SxSsnSTYRilgCA2WHhKv1C_0Tipb9X',
+    scores: [
+      "2",
+      "3",
+      "3",
+      "1",
+      "2",
+      "3",
+      "2",
+      "2",
+      "1",
+      "2"
+    ]
+  },
+  {
+    name: 'Box',
+    image: 'http://www.jeffbots.com/logansrun6.jpg',
+    scores: [
+      "5",
+      "1",
+      "1",
+      "3",
+      "2",
+      "1",
+      "2",
+      "2",
+      "5",
+      "2"
+    ]
+  },
+  {
+    name: 'Steel',
+    image: 'https://i.pinimg.com/736x/37/ce/46/37ce46d5c873bd1439337e020dfc60af--sci-fi-horror-movies-rude-mechanicals.jpg',
+    scores: [
+      "3",
+      "4",
+      "4",
+      "3",
+      "5",
+      "4",
+      "5",
+      "5",
+      "4",
+      "3"
+    ]
+  },
+  {
+    name: 'The Annihlatons',
+    image: 'https://cdn.pastemagazine.com/www/articles/53-Best-100-Robots-in-Film-Annihilatons.jpg',
+    scores: [
+      "3",
+      "1",
+      "1",
+      "5",
+      "3",
+      "1",
+      "5",
+      "5",
+      "2",
+      "5"
+    ]
+  },
+  {
+    name: 'Venusian',
+    image: 'https://s-media-cache-ak0.pinimg.com/originals/60/36/1f/60361ff518b4658126a3f0181d662923.jpg',
+    scores: [
+      "2",
+      "4",
+      "4",
+      "5",
+      "1",
+      "1",
+      "3",
+      "3",
+      "3",
+      "4"
+    ]
+  },
+  {
+    name: 'Maria',
+    image: 'https://monstersfilmandlit.files.wordpress.com/2013/11/maria.jpg',
+    scores: [
+      "3",
+      "3",
+      "3",
+      "5",
+      "4",
+      "5",
+      "3",
+      "4",
+      "3",
+      "5"
+    ]
+  },
+  {
+    name: 'The Mechanical Men',
+    image: 'https://i.ytimg.com/vi/ruTNAbX5TDI/hqdefault.jpg',
+    scores: [
+      "1",
+      "3",
+      "5",
+      "2",
+      "3",
+      "5",
+      "4",
+      "5",
+      "3",
+      "5"
+    ]
+  },
+  {
+    name: 'Kronos',
+    image: 'http://www.monsterbashnews.com/Morepics/Kronos-Eater.jpg',
+    scores: [
+      "1",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "5",
+      "3",
+      "1"
+    ]
+  }
+];
+
+module.exports = friendList;
